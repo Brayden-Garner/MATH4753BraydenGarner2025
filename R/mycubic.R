e397adc09a9295619af33c4efcac5fd5e4fa53dc
Brayden Garner <bgarner@ou.edu>
2025-02-07 23:49:07 -0600

    d

---
+++ R/mycubic.R
@@ -0,0 +1,12 @@
+#' Mycube function
+#'
+#' @param x A quantitative vector
+#'
+#' @returns A quantitative vector with cubed components
+#' @export
+#'
+#' @examples
+#' mycubic(1:10)
+mycubic <- function(x){
+  x^3
+}
