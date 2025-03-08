15db61810adaea8819cf7fbf973d51e0c8c20caa
Brayden Garner <bgarner@ou.edu>
2025-03-07 22:15:04 -0600

    Lab7

---
+++ R/fire.R
@@ -0,0 +1,13 @@
+#' Fire Damage Data Set
+#'
+#' This data set contains information about fire incidents, including the distance of the fire
+#' from a specific location and the resulting damage.
+#'
+#' @format ## `fire damage`
+#' A data frame with 15 observations and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{Distance to the fire.}
+#'   \item{DAMAGE}{Damage caused by the fire}
+#' }
+#' @source Unknown
+"fire"
